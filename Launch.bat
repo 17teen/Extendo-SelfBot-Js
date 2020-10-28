7adb52dbc2f94eb2eb372d9bcb525fde3eb5c3d1
7teen <71920969+17teen@users.noreply.github.com>
2020-10-28 17:06:13 +0000

    Extendo v2(updated)

--- Launch.bat
+++ Launch.bat
@@ -1,5 +1,5 @@
 @echo off
-title Launching Modules v.1.1.0
+title Launching Modules v.2.1.0
 :top
 cls
 node launch.js
@@ -8,6 +8,8 @@ pause
     call npm install node-fetch
     call npm install @discordjs/uws@^10.149.0
     call npm install discord.js@11.5.0
+    call npm install random-puppy
+    call npm install figlet
     call npm install chalk
 pause
 exit
